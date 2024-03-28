ace87a9f075bf0fdff5c4303afad97c97bf217fa
RamaiyaA <ramaiya.a@cisinlabs.com>
2024-03-28 18:20:45 +0530

    ading env port on index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import dbConection from "./db/db.js";
 import { app } from "./app.js";
 
 
-const port = 3002;
+
 dotenv.config({ path: "./env" });
 
 dbConection();
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+	console.log(`Example app listening on port ${process.env.PORT}`);
 });
